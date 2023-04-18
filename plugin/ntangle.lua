4876f97305705f9f4184d7b3f970979392e92a49
jbyuki <jyburkhard@hotmail.com>
2023-04-18 09:54:14 +0200

    replace .vim file

---
+++ plugin/ntangle.lua
@@ -0,0 +1,7 @@
+vim.cmd [[autocmd BufWrite *.t lua require"ntangle".tangle_buf()]]
+
+vim.cmd [[command! TangleBuildCache lua require"ntangle".build_cache(fnamemodify("~/tangle_cache.txt", ":p"))]]
+
+vim.cmd [[command! TangleAll lua require"ntangle".tangle_all()]]
+
+vim.cmd [[command! TangleWithComments lua require"ntangle".tangle_buf_with_comments()]]
--- plugin/ntangle.vim
+++
@@ -1,8 +0,0 @@
-" Generated using ntangle.nvim
-autocmd BufWrite *.t lua require"ntangle".tangle_buf()
-
-command! TangleBuildCache lua require"ntangle".build_cache(fnamemodify("~/tangle_cache.txt", ":p"))
-
-command! TangleAll lua require"ntangle".tangle_all()
-
-command! TangleWithComments lua require"ntangle".tangle_buf_with_comments()
